3915cfb7ff218c33f3c8c8de4e6a1d8a27d3a873
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-11-19 17:25:54 -0500

    Updates

---
+++ precache-manifest.3a4cf464d1274fc615bf5275bea1d37a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a1bc39042ecf940668",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.24a1bc39.js"
+  },
+  {
+    "revision": "7b5ada7642eba65749ad",
+    "url": "/tic-tac-toe-react/static/js/main.7b5ada76.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/tic-tac-toe-react/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "7b5ada7642eba65749ad",
+    "url": "/tic-tac-toe-react/static/css/main.9627c6e0.chunk.css"
+  },
+  {
+    "revision": "9f55860a5601dbd61d853328bd93954b",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
